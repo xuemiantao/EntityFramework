09cdd7fa9ae7417c6a9fa5501ba6c0082b7b769a
Christopher Harrison <chrhar@microsoft.com>
2015-01-24 10:44:43 -0800

    Decks two and three

---
+++ Work/Samples/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samples.Models
+{
+	public class Album
+	{
+		public int AlbumID { get; set; }
+		public string Name { get; set; }
+		public int ArtistID { get; set; }
+		public string Artist { get; set; }
+	}
+}
---
+++ Work/Samples/Models/MusicStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Samples.Models
+{
+	public class MusicStoreDbContext : DbContext
+	{
+		public DbSet<Artist> Artists { get; set; }
+		public DbSet<Album> Albums { get; set; }
+	}
+}
