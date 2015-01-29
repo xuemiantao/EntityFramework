b96a98abc91f7a5c0414e54ac393b2c404790533
adamtuliper <adam.tuliper@gmail.com>
2015-01-29 02:22:31 -0800

    Added code demos and notes

---
+++ AdamDemos/ConcurrencyDetectionAndUserChoice/ConcurrencyDetectionAndUserChoice/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConcurrencyDetectionAndUserChoice
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+    }
+}
---
+++ AdamDemos/ConcurrencyDetectionAndUserChoice/ConcurrencyDetectionAndUserChoice/MusicContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ConcurrencyDetectionAndUserChoice
+{
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
